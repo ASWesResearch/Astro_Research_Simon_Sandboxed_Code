bc64880d62ab505a165e00d70d30ad5fb11b76b9
Anthony Santini <asantini@mando.astro.wesleyan.edu>
2023-02-13 21:22:49 -0500

    Updated Specextract/specextract_from_csc.py to Python 3, Modified Specextract/specextract_from_csc.py to work on Mando, Added fuctionality to Specextract/specextract_from_csc.py where ObsIDs that alreay exist can be skipped entirely without invoking any ciao tools with clobber=no, Modified fucntion calls.

--- Specextract/specextract_from_csc.py
+++ Specextract/specextract_from_csc.py
@@ -9,6 +9,7 @@ This version of the code is now modifed by Anthony Santini (Wesleyan 2019 Master
 
 """
 import os
+import sys
 from ciao_contrib.runtool import *
 import ciao_contrib.runtool as rt
 import numpy as np
@@ -16,8 +17,17 @@ import glob
 import re
 from multiprocessing import Pool
 #from done_email import when_done
-
-def spec(obsid):
+path="/opt/xray/anthony/Research_Git/"
+sys.path.append(os.path.abspath(path))
+from ObsID_From_CSV_Query import ObsID_From_CSV_Query #Ant: This should be changed as to not depend on the Research_Git directory
+#Constants:
+#Root_Path="/Volumes/"
+Root_Path="/opt/"
+#Reg_Path="/Volumes/xray/anthony/Research_Git/Nearest_Raytraced_Neighbor_Calc/"
+Reg_Path="/Volumes/expansion/"
+#Outpath="/Volumes/xray/anthony/Simon_Sandboxed_Code/Specextract/"
+Outpath="/Volumes/expansion/"
+def spec(obsid, Clobber_Bool=False):
     Error_List=[]
     with rt.new_pfiles_environment(ardlib=True):
         with new_tmpdir() as tmpdir:
@@ -49,21 +59,39 @@ def spec(obsid):
             print str(num) + " sources to be extracted..."
             """
             obsid = str(obsid)
-            print "Beginning flux extraction for obsid " + obsid + "..."
-            print "Getting name of evt2 file..."
+            #/Volumes/expansion/extracted_spectra/10125/
+            if(Clobber_Bool==False):
+                Output_Path="/Volumes/expansion/extracted_spectra/"+str(obsid)+"/"
+                Output_Path_L=glob.glob(Output_Path)
+                if(len(Output_Path_L)>0):
+                    print(str(obsid)+" already exists and Clobber is set to False!")
+                    return
+            print("Beginning flux extraction for obsid " + obsid + "...")
+            print("Getting name of evt2 file...")
             ##name = glob.glob("chandra_from_csc/" + obsid + "/primary/*_evt2.fits")
-            Evt2_Fpath_L = glob.glob("/Volumes/xray/simon/all_chandra_observations/" + obsid + "/primary/*_evt2.fit*")
+            #Evt2_Fpath_L = glob.glob("/Volumes/xray/simon/all_chandra_observations/" + obsid + "/primary/*_evt2.fit*")
+            Evt2_Fpath_L = glob.glob("/Volumes/expansion/ObsIDs/" + obsid + "/new/*evt2.fit*")
+            if(len(Evt2_Fpath_L)==0):
+                print(str(obsid)+" has no reprocessed Evt2 file!")
+                Evt2_Fpath_L = glob.glob("/Volumes/expansion/ObsIDs/" + obsid + "/*evt2.fit*")
+            if(len(Evt2_Fpath_L)==0):
+                print(str(obsid)+" HAS NO MATCHING ObsIDs!!!!")
+                return
             Evt2_Fpath=Evt2_Fpath_L[0]
-            print "Evt2_Fpath: ", Evt2_Fpath
+            print("Evt2_Fpath: ", Evt2_Fpath)
             ##print "Shortening name..."
             #Ant: This is done only for an exact directory on a computer not used anymore. It must be modified to create the shortened filename from an arbitary filepath
             ##name = name[0][-24:]
             name_L=Evt2_Fpath.split("/")
-            print "name_L: ", name_L
+            print("name_L: ", name_L)
             name=name_L[len(name_L)-1]
-            print name
-            Hybrid_Filepath="/Volumes/xray/anthony/Research_Git/Nearest_Raytraced_Neighbor_Calc/Hybrid_Regions/"+str(obsid)+"/"+str(obsid)+"_Nearest_Neighbor_Hybrid.reg"
-            Hybrid_File=open(Hybrid_Filepath)
+            print(name)
+            Hybrid_Filepath=Reg_Path+"Hybrid_Regions/"+str(obsid)+"/"+str(obsid)+"_Nearest_Neighbor_Hybrid.reg"
+            try:
+                Hybrid_File=open(Hybrid_Filepath)
+            except:
+                print(str(obsid)+" has no source region file!")
+                return
             Hybrid_Region_Str=Hybrid_File.read()
             Header_String='# Region file format: DS9 version 3.0\nglobal color=blue font="helvetica 10 normal" select=1 edit=1 move=1 delete=1 include=1 fixed=0\n'
             Hybrid_Region_Str_L=Hybrid_Region_Str.split(Header_String)
@@ -74,11 +102,11 @@ def spec(obsid):
             #print "len(Hybrid_Region_Str_Reduced_L) Before Pop: ", len(Hybrid_Region_Str_Reduced_L)
             Hybrid_Region_Str_Reduced_L.pop(len(Hybrid_Region_Str_Reduced_L)-1)
             num=len(Hybrid_Region_Str_Reduced_L)
-            print "num: ", num
+            print("num: ", num)
             ##print "Unlearning specextract..."
             ##os.system("punlearn specextract")
             #print "Getting system path..."
-            Hybrid_BG_Filepath="/Volumes/xray/anthony/Research_Git/Nearest_Raytraced_Neighbor_Calc/Hybrid_Regions/"+str(obsid)+"/"+str(obsid)+"_Nearest_Neighbor_Hybrid_Background.reg"
+            Hybrid_BG_Filepath=Reg_Path+"Hybrid_Regions/"+str(obsid)+"/"+str(obsid)+"_Nearest_Neighbor_Hybrid_Background.reg"
             Hybrid_BG_File=open(Hybrid_BG_Filepath)
             Hybrid_BG_Region_Str=Hybrid_BG_File.read()
             Header_String='# Region file format: DS9 version 3.0\nglobal color=blue font="helvetica 10 normal" select=1 edit=1 move=1 delete=1 include=1 fixed=0\n'
@@ -91,17 +119,17 @@ def spec(obsid):
             Hybrid_BG_Region_Str_Reduced_L.pop(len(Hybrid_BG_Region_Str_Reduced_L)-1)
             #for Hybrid_BG_Region_Str in Hybrid_BG_Region_Str_Reduced_L: #Ant: I need to modify this so that it itterates through the Hybrid source background regions as well!
             specextract = rt.make_tool("specextract")
-            print "specextract Tool Object Before:\n", specextract
+            print("specextract Tool Object Before:\n", specextract)
             #specextract.tmpdir = tmpdir
-            print "tempdir: " , tmpdir
+            print("tempdir: " , tmpdir)
             specextract.tmpdir = tmpdir
-            print "specextract Tool Object After:\n", specextract
+            print("specextract Tool Object After:\n", specextract)
             #for i in range(1,num+1):
             for i in range(0,num):
                 Hybrid_Region_Str=Hybrid_Region_Str_Reduced_L[i]
-                print "Hybrid_Region_Str: ", Hybrid_Region_Str
+                print("Hybrid_Region_Str: ", Hybrid_Region_Str)
                 Hybrid_Region_Str_L=re.split("[(),]", Hybrid_Region_Str)
-                print "Hybrid_Region_Str_L: ", Hybrid_Region_Str_L
+                print("Hybrid_Region_Str_L: ", Hybrid_Region_Str_L)
                 """
                 x_str=Hybrid_Region_Str_L[1]
                 x=float(x_str)
@@ -126,32 +154,32 @@ def spec(obsid):
                 print inst
                 """
                 #Ant: Reducing the Hybrid_Region_Str to a region shape
-                print "Extracting soft X-rays for source " + str(i+1) + " in obsid " + obsid + "..."
+                print("Extracting soft X-rays for source " + str(i+1) + " in obsid " + obsid + "...")
                 Hybrid_Region_Str_Reduced_Split_L=re.split("[; ]",Hybrid_Region_Str)
-                print "Hybrid_Region_Str_Reduced_Split_L: ", Hybrid_Region_Str_Reduced_Split_L
+                print("Hybrid_Region_Str_Reduced_Split_L: ", Hybrid_Region_Str_Reduced_Split_L)
                 Hybrid_Region_Str_Reduced=Hybrid_Region_Str_Reduced_Split_L[1]
-                print "Hybrid_Region_Str_Reduced: ", Hybrid_Region_Str_Reduced
+                print("Hybrid_Region_Str_Reduced: ", Hybrid_Region_Str_Reduced)
                 #Ant: Parsing the parsing the Hybrid source background region file to in order to feed that input into specextract
-                print "i: ", i
+                print("i: ", i)
                 j=i
-                print "j Before: ", j
+                print("j Before: ", j)
                 """
                 if(j%2!=0):
                     #j=j+1 #Ant: I lost a week to this bug...
                     j=2*j
                 """
                 j=2*j
-                print "j After: ", j
+                print("j After: ", j)
                 Hybrid_BG_Region_Outer_R_Str=Hybrid_BG_Region_Str_Reduced_L[j]
                 Hybrid_BG_Region_Outer_R_Str_Reduced_L=re.split("[; ]",Hybrid_BG_Region_Outer_R_Str)
                 Hybrid_BG_Region_Outer_R_Str_Reduced=Hybrid_BG_Region_Outer_R_Str_Reduced_L[1]
-                print "Hybrid_BG_Region_Outer_R_Str_Reduced: ", Hybrid_BG_Region_Outer_R_Str_Reduced
+                print("Hybrid_BG_Region_Outer_R_Str_Reduced: ", Hybrid_BG_Region_Outer_R_Str_Reduced)
                 Hybrid_BG_Region_Inner_R_Str=Hybrid_BG_Region_Str_Reduced_L[j+1]
                 Hybrid_BG_Region_Inner_R_Str_Reduced_L=re.split("[; ]",Hybrid_BG_Region_Inner_R_Str)
                 Hybrid_BG_Region_Inner_R_Str_Reduced=Hybrid_BG_Region_Inner_R_Str_Reduced_L[1]
-                print "Hybrid_BG_Region_Inner_R_Str_Reduced: ", Hybrid_BG_Region_Inner_R_Str_Reduced
+                print("Hybrid_BG_Region_Inner_R_Str_Reduced: ", Hybrid_BG_Region_Inner_R_Str_Reduced)
                 Hybrid_BG_Region=Hybrid_BG_Region_Outer_R_Str_Reduced+Hybrid_BG_Region_Inner_R_Str_Reduced
-                print "Hybrid_BG_Region: ", Hybrid_BG_Region
+                print("Hybrid_BG_Region: ", Hybrid_BG_Region)
                 # print "specextract " + obsid + " " + str(i)
                 # print "Assigning new defaults to specextract for source number " + str(i) + "..."
                 # print "Assigning infile..."
@@ -169,8 +197,8 @@ def spec(obsid):
                 # print "...done with source number " + str(i) + "!"
 
                 #ALTERNATELY (USE THIS INSTEAD SO SPECEXTRACT DOES NOT BUG YOU FOR CONFIRMATIONS):
-                print "Running specextract..."
-                print "Source " + str(i+1) + " of " + str(num) + " for obsid " + str(obsid) + "..."
+                print("Running specextract...")
+                print("Source " + str(i+1) + " of " + str(num) + " for obsid " + str(obsid) + "...")
                 """
                 os.system("specextract \
                 infile='chandra_from_csc/" + str(obsid) + "/primary/" + str(name) + "[sky=region(/Volumes/xray/spirals/trace/" + str(obsid) + "/" + str(i) + ".reg)]' \
@@ -204,23 +232,29 @@ def spec(obsid):
                 """
 
                 #Ant Note: dmcoords(infile=str(evtfpath),chipx=BG_X, chipy=BG_Y, chip_id=Chip_ID_Test, option='chip', verbose=0)
+                """
                 Infile_Str="/Volumes/xray/simon/all_chandra_observations/" + str(obsid) + "/primary/" + str(name) + "[sky="+Hybrid_Region_Str_Reduced+"]"
                 Bkgfile_Str="/Volumes/xray/simon/all_chandra_observations/" + obsid + "/primary/" + str(name) + "[sky="+Hybrid_BG_Region+"]"
                 Outroot_Str="/Volumes/xray/anthony/Simon_Sandboxed_Code/Specextract/extracted_spectra/" + str(obsid) +"/"+ "extracted_spectra_" + str(obsid) + "_" + str(i+1)
                 Outroot_Str_Test="/Volumes/xray/anthony/Simon_Sandboxed_Code/Specextract/extracted_spectra_Test/" + str(obsid) +"/"+ "extracted_spectra_" + str(obsid) + "_" + str(i+1)
+                """
                 #specextract(infile=Infile_Str, bkgfile=Bkgfile_Str, outroot=Outroot_Str, correctpsf="yes", weight="no", clobber="yes", verbose=1)
                 #Ant Note: ah = rt.make_tool("asphist")
+                Infile_Str="/Volumes/expansion/ObsIDs/" + str(obsid) + "/new/" + str(name) + "[sky="+Hybrid_Region_Str_Reduced+"]"
+                Bkgfile_Str="/Volumes/expansion/ObsIDs/" + obsid + "/new/" + str(name) + "[sky="+Hybrid_BG_Region+"]"
+                Outroot_Str=Outpath+"extracted_spectra/" + str(obsid) +"/"+ "extracted_spectra_" + str(obsid) + "_" + str(i+1)
+                Outroot_Str_Test=Outpath+"extracted_spectra_Test/" + str(obsid) +"/"+ "extracted_spectra_" + str(obsid) + "_" + str(i+1)
                 """
                 specextract = rt.make_tool("specextract")
                 print "specextract Tool Object: ", specextract
                 #specextract.tmpdir = tmpdir
                 print "tempdir: " , tmpdir
                 """
-                print "Infile_Str: ", Infile_Str
-                print "Bkgfile_St: ", Bkgfile_Str
+                print("Infile_Str: ", Infile_Str)
+                print("Bkgfile_St: ", Bkgfile_Str)
                 #print "Outroot_Str_Test: ", Outroot_Str_Test
-                print "Outroot_Str: ", Outroot_Str
-                print "tempdir In: ", tmpdir
+                print("Outroot_Str: ", Outroot_Str)
+                print("tempdir In: ", tmpdir)
                 #Temp_Files = glob.glob(tmpdir+"| grep 'asphist'")
                 #Temp_Files = glob.glob(tmpdir+"/*")
                 #print "Temp_Files:\n", Temp_Files
@@ -231,12 +265,15 @@ def spec(obsid):
                 """
                 try:
                     #specextract(infile=Infile_Str, bkgfile=Bkgfile_Str, outroot=Outroot_Str_Test, correctpsf="yes", weight="no", clobber="yes", tmpdir=tmpdir, verbose=1)
-                    specextract(infile=Infile_Str, bkgfile=Bkgfile_Str, outroot=Outroot_Str, correctpsf="yes", weight="no", clobber="yes", tmpdir=tmpdir, verbose=1)
+                    if(Clobber_Bool):
+                        specextract(infile=Infile_Str, bkgfile=Bkgfile_Str, outroot=Outroot_Str, correctpsf="yes", weight="no", clobber="yes", tmpdir=tmpdir, verbose=1)
+                    else:
+                        specextract(infile=Infile_Str, bkgfile=Bkgfile_Str, outroot=Outroot_Str, correctpsf="yes", weight="no", clobber="no", tmpdir=tmpdir, verbose=1)
                 except:
                     Cur_Error_L=[obsid, Infile_Str, Bkgfile_Str, Outroot_Str_Test, tmpdir]
                     Error_List.append(Cur_Error_L)
 
-    print "Error_List: ", Error_List
+    print("Error_List: ", Error_List)
 
 
 ##if __name__ == '__main__':
@@ -292,7 +329,7 @@ def Driver(obsid_L):
     """
     #Ant: Multiprocessing to make it stop
     if __name__ == '__main__':
-        P = Pool(8)
+        P = Pool()
         P.map(spec, obsid_L)
 
     #when_done("specextract") #Ant: Disabled to not accidently email Simon
@@ -312,7 +349,9 @@ def Main():
     #Driver([1972])
     #Parallelization
     #[4742, 2039, 3150, 2030, 4743, 5197, 11784, 9552]
-    Driver([4742, 2039, 3150, 2030, 4743, 5197, 11784, 9552])
-
+    #Driver([4742, 2039, 3150, 2030, 4743, 5197, 11784, 9552])
+    #ObsID_L=ObsID_From_CSV_Query.Read_ObsIDs(Remove_Unarchived=True)
+    ObsID_L=ObsID_From_CSV_Query.Read_ObsIDs()
+    Driver(ObsID_L)
 
 Main()
